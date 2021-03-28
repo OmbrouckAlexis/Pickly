467ba624c0f0088d1477dc52cded6d119e2259ae
Alexis <alexis.ombrouck@hotmail.fr>
2021-03-28 20:09:08 +0200

    deuxieme

--- resources/views/firstcontroller/search.blade.php
+++ resources/views/firstcontroller/search.blade.php
@@ -1,11 +1,18 @@
 @extends("layouts.template")
 
 @section('content')
-        
+
+<div id="search-section">
+
+<div class="title-search">
 <h3>Les photos</h3>
+</div>
  @include('_songs', ["songs" => $songs])       
 
+<div class="title-search">
 <h3>Les Utilisateurs</h3>
+</div>
+
 @foreach($users as $u)
     <div>
         <h2><a href="/user/{{$u->id}}">{{$u->name}} </a></h2>
@@ -13,4 +20,6 @@
         <h3>{{$u->TheyLikeMe()->count()}} Abonnés</h3>
     </div>
 @endforeach
+
+</section>
 @endsection
